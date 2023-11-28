c928def14a01a4f2b79bd5d6b917c0595e04df02
{{Valeria <lerycia2005@gmail.com>
2023-11-28 22:15:59 +0200

    finish work

---
+++ Завдання 1/Задача 1/Задача 1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void replace_negatives(std::vector<int>& arr) {
+    if (arr.empty()) {
+        return;
+    }
+
+    auto max_val_idx = std::max_element(arr.begin(), arr.end()) - arr.begin();
+
+    for (size_t i = 0; i < max_val_idx; ++i) {
+        if (arr[i] < 0) {
+            arr[i] = 0;
+        }
+    }
+}
+
+int main() {
+    // Введення розміру масиву та елементів
+    int size;
+    std::cout << "Enter the size of the array: ";
+    std::cin >> size;
+
+    std::vector<int> array(size);
+    std::cout << "Enter the elements of the array:\n";
+    for (int i = 0; i < size; ++i) {
+        std::cin >> array[i];
+        replace_negatives(array);
+    }
+
+    for (const auto& num : array) {
+        std::cout << num << ' ';
+    }
+
+    return 0;
+}
---
+++ Завдання 1/Задача 5/Задача 5.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "n= ";
+    cin >> n;
+
+    if (n < 1) {
+        cout << "try again";
+    }
+    else {
+        double array[n];
+
+        for (int i = 0; i < n; i++) {
+            cin >> array[i];
+        }
+
+        double a, suma = 0;
+        int k = 0, j = 0;
+        bool check = false;
+        cout << "A:=";
+        cin >> a;
+
+        for (int i = 0; i < n; i++) {
+            if (array[i] < a) k++;
+
+            if (array[i] < 0) { j = i; check = true; }
+        }
+
+        if (check) {
+            double temp, temp2;
+            for (int i = j + 1; i < n; i++) {
+                temp = array[i];
+                modf(temp, &temp2);
+                suma = suma + temp2;
+            }
+        }
+        cout << "less than A:=" << k << " amount after last<0:=" << suma;
+
+        double max = array[0];
+
+        for (int i = 0; i < n; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        double array2[n], diff = max * 0.2;;
+        int b = 0;
+
+
+        for (int i = 0; i < n; i++) {
+
+            if ((array[i] <= (max + diff)) && (array[i] >= (max - diff))) {
+                array2[b] = array[i];
+                b++;
+            }
+        }
+
+        for (int i = b - 1; i < n; i++) {
+            if ((array[i] >= (max + diff)) || (array[i] <= (max - diff))) {
+                array2[b] = array[i];
+                b++;
+            }
+        }
+
+        cout << '\n' << "Array: ";
+        for (int i = 0; i < n; i++) {
+            cout << array2[i] << " ";
+        }
+
+    }
+    return 0;
+}
